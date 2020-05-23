d6d5f9f5a7d85e114139f4359c04ea69bf90a1d2
夏俊炎 <h0730303779@hotmail.com>
2020-05-23 12:54:48 +0800

    1

---
+++ SimpleWinForms_netcore/FrmMain.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinForms_netcore
+{
+    public partial class FrmMain : RibbonForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
